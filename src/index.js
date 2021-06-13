394545797db5d7bc5e0edb64a2bec2b93c208557
vuonghoccode <vuong260902@gmail.com>
2021-06-13 16:07:52 +0700

    install prettier lint-staged husky

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
-const port = 3000;
+    const port = 3000;
 
-const route = require('./routes');
+        const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
 
                 app.use(
                     express.urlencoded({
